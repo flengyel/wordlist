9203744cd84e76c8257953e75f744b2227b63b1e
Florian Lengyel <florian.lengyel@gmail.com>
2013-11-22 03:32:12 +0000

    Additional comments.

--- rest.py
+++ rest.py
@@ -5,22 +5,25 @@ from   reverseproxied import ReverseProxied
 from   string import rstrip
 from   random import sample
 
-# load the wordlist
+# load the set of words 
 wordset = set()
 with open('wordlist/wordlist') as fp:
-      for line in fp:
-	line = line.rstrip()
-	if len(line) == 0:
-	  continue
-        wordset.add(line) 
+  for line in fp:
+    line = line.rstrip()
+    if len(line) == 0:
+      continue
+    wordset.add(line) 
 
 app = Flask(__name__)
 app.wsgi_app = ReverseProxied(app.wsgi_app)
 
 @app.route('/')
 def randomWord():
-    return json.dumps({'Word': sample(wordset,1)[0]})
+  """Return the JSON of a python map (in JavaScript, an object) 
+     with the key 'Word' and value uniformly sampled from the 
+     set of words from the file wordlist. The function sample 
+     returns a list of length 1; we take its first element."""
+  return json.dumps({'Word': sample(wordset,1)[0]})
 
 if __name__ == '__main__':
-    #app.debug=True
-    app.run(host='0.0.0.0')
+  app.run(host='0.0.0.0')
